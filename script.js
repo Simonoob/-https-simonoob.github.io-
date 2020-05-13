e90fc8759ee185802c1b646aad002c4dcb52b679
Simone Fiorentini <simonefiorentini1@gmail.com>
2020-05-13 17:24:06 +0300

    +visual interface and buttons, playerSelection value not working properly(changing to 0 in play() function)

--- script.js
+++ script.js
@@ -1,62 +1,90 @@
-//computer's play ---------------------------------
-const selections = ["rock","paper","scissors"];
-let randomSelection = (selections)=> selections[Math.floor(Math.random() * selections.length)];
-let printComputerPlay=(computerSelection)=>console.log("the computer plays",computerSelection);
 
-
-//match function ----------------------------
-function match(computerSelection,playerSelection){
-        if ((computerSelection=="rock" && playerSelection=="rock") || (computerSelection=="paper" && playerSelection=="paper") || (computerSelection=="scissors" && playerSelection=="scissors")){
-            console.log("It's a draw");
-        }
-        else if ((computerSelection=="rock" && playerSelection=="scissors") || (computerSelection=="paper" && playerSelection=="rock") || (computerSelection=="scissors" && playerSelection=="paper")){
-            console.log("Computer wins");
-            computerWins+=1;
-        }
-        else if ((computerSelection=="rock" && playerSelection=="paper") || (computerSelection=="paper" && playerSelection=="scissors") || (computerSelection=="scissors" && playerSelection=="rock")){
-            console.log("Player wins");
-            playerWins+=1;
-        }
-        else{
-            console.log("I can't detect the winner");
-        }
-}
-
-
-//player's play and match -------------------------------------
+//variables ------------------------------
 let matches=0;
 let playerWins=0;
 let computerWins=0;
-function printResult(playerSelection,computerSelection,matches){
-    
+let playerSelection=undefined;
+let computerSelection=undefined;
+let winner=undefined;
+
+//computer's selection ---------------------------------
+const selections = ["rock","paper","scissors"];
+let randomSelection = (selections)=> selections[Math.floor(Math.random() * selections.length)];
+
+//player's selection + match function call -------------------------------------
+function toRock(playerSelection){
+    playerSelection="rock";
+    alert(`selection ${playerSelection}`);//-+-+-++-+-+-+-+-+--+-+-+-+-+-+-+-
+    play(matches,playerSelection,computerSelection,playerWins,computerWins);
+    alert(`selection ${playerSelection}`);//--+-+-++-+-+-+-+-+--+-+-+-+-+-+-+-
+}
+function toPaper(playerSelection){
+    playerSelection="paper";
+
+    play(matches,playerSelection,computerSelection,playerWins,computerWins);
+}
+function toScissors(playerSelection){
+    playerSelection="scissors";
+    play(matches,playerSelection,computerSelection,playerWins,computerWins);
+
 }
 
-while (true){
-    while (matches<5){
-    //let computerSelection = randomSelection(selections);
-    let computerSelection="rock"; //delete here for final project
-    let playerSelection = prompt("write rock, paper or scissors");
-    playerSelection=playerSelection.toLowerCase();
-    let printPlayerPlay=(playerSelection)=> console.log("you play",playerSelection);
+//functions defining winner and printing results ----------------------------
+function getWinner(computerSelection,playerSelection){
+        if ((computerSelection=="rock" && playerSelection=="rock") || (computerSelection=="paper" && playerSelection=="paper") || (computerSelection=="scissors" && playerSelection=="scissors")){
+            winner="Draw";
+        }
+        else if ((computerSelection=="rock" && playerSelection=="scissors") || (computerSelection=="paper" && playerSelection=="rock") || (computerSelection=="scissors" && playerSelection=="paper")){
+            winner="Computer wins";
+            computerWins+=1;
+        }
+        else if ((computerSelection=="rock" && playerSelection=="paper") || (computerSelection=="paper" && playerSelection=="scissors") || (computerSelection=="scissors" && playerSelection=="rock")){
+            winner="You win";
+            playerWins+=1;
+        }
+}
+function printResults(matches,playerSelection,computerSelection,winner,playerWins,computerWins){
+    document.getElementById("player").textContent=`You play ${playerSelection}`;
+    document.getElementById("computer").textContent =`The computer plays ${computerSelection}`;
+    document.getElementById("winner").textContent=winner;
+    document.getElementById("player-wins").textContent=`You won ${playerWins}`;
+    document.getElementById("computer-wins").textContent=`Computer won ${computerWins}`;
+    document.getElementById("rounds").textContent=`Rounds played: ${matches}/5`;
+}
 
-    if  (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
-        printPlayerPlay(playerSelection);
-        printComputerPlay(computerSelection);
-        match(computerSelection,playerSelection);
-        matches+=1;
-        console.log("turs played:",matches,"/ 5");
+//function to reset the game ------------------------------------
+function reset(playerWins,computerWins,matches){
+    playerSelection=undefined;
+    matches=0;
+    playerWins=0;
+    computerWins=0;
+}
+//function to play -------------------------------------
+function play(playerSelection,computerSelection,playerWins,computerWins,matches){
+    alert(`selection ${playerSelection}`);//--+-+-++-+-+-+-+-+--+-+-+-+-+-+-+-
+    if (matches<5){
+        alert(`selection ${playerSelection}`);//--+-+-++-+-+-+-+-+--+-+-+-+-+-+-+-
+        if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
+            alert(`selection ${playerSelection}`);
+            matches+=1;
+            let computerSelection = randomSelection(selections);
+            getWinner(computerSelection,playerSelection);
+            printResults(matches,playerSelection,computerSelection,winner,playerWins,computerWins);
+        }
+        else{
+            alert("something went wrong\nPlease try again");
+        }
     }
+    if (matches==5){
+        if (playerWins==computerWins){
+            document.getElementById("player").textContent=``;
+            document.getElementById("computer").textContent =`The game is a draw`;
+            document.getElementById("winner").textContent=`Press reset to play again`;
+        }
+        else {
+            playerWins>computerWins? (console.log("Player wins the game"), alert("Player wins the game\nLet's play once more")): console.log("playing");
+            computerWins>playerWins? (console.log("Computer wins the game"),alert("Computer wins the game\nLet's play once more")):console.log("playing");
+        }
 
-    else {
-        alert("Something went wrong..\nMake sure to write only one word");
     }
-}
-if (playerWins==computerWins){
-    console.log("the game is a draw"), alert("the game is a draw\nLet's play once more");
-}
-else (playerWins>computerWins) ? (console.log("Player wins the game"), alert("Player wins the game\nLet's play once more")): (console.log("Computer wins the game"),alert("Computer wins the game\nLet's play once more"));
-
-matches=0;
-playerWins=0;
-computerWins=0;
 }
